c124de5e8447d8fffdc691a705748d9225e7ee62
Thiago <thiagorochasan@gmail.com>
2023-10-03 16:48:15 -0300

    Enviando front end

---
+++ src/pages/category/index.tsx
@@ -0,0 +1,81 @@
+import { useState, FormEvent } from "react"
+import Head from "next/head"
+import { Header } from "../../components/Header"
+import styles from './styles.module.scss'
+import { setupAPIClient } from "../../services/api"
+import { toast } from "react-toastify"
+import { canSSRAuth } from "../../utils/canSSRAuth"
+
+
+export default function Category(){
+
+    const [name, setName] = useState('');
+
+    async function handleRegister(event: FormEvent){
+        event.preventDefault();
+
+        if(name ==''){
+            return;
+        }
+
+        const apiClient = setupAPIClient();
+
+        await apiClient.post('/category', {
+            name: name
+        })
+
+
+        toast.success('Categoria cadastrada com sucesso!');
+        setName('');
+
+    }
+    
+
+
+    return(
+        <>
+        
+        <Head>
+            <title>Nova categoria</title>
+        </Head>
+        <div>
+
+            <Header></Header>
+
+            <main className={styles.container}>
+                <h1>Cadastrar categorias</h1>
+
+
+                <form className={styles.form}  onSubmit={handleRegister}>
+
+                    <input
+                    type="text"
+                    placeholder="Digite o nome da catagoria"
+                    className={styles.input}
+                    value={name}
+                    onChange={ (e) => setName(e.target.value) }
+                    ></input>
+
+                    <button type="submit" className={styles.buttonAdd}>
+                        Cadastrar
+                    </button>
+
+                </form>
+
+            </main>
+
+            
+        </div>
+        
+        </>
+    )
+    
+}
+
+
+export const getServerSideProps = canSSRAuth( async (ctx) => {
+
+    return{
+        props: {}
+    }
+})
---
+++ src/pages/index.tsx
@@ -0,0 +1,90 @@
+
+import{useContext, FormEvent, useState} from 'react';
+
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../../styles/home.module.scss';
+import logoImg from '../../public/logo.svg';
+import { Input } from '../components/ui/input';
+import { Button } from '../components/ui/Button';
+
+import { AuthContext } from '../contexts/AuthContext';
+import { toast } from 'react-toastify';
+
+import Link  from 'next/link';
+import { canSSRGuest } from '../utils/canSSRGuest';
+
+export default function Home() {
+
+  const {signIn } = useContext(AuthContext)
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+
+  async function handleLogin(event: FormEvent){
+    event.preventDefault();
+
+    if (email == '' || password == '') {
+      toast.warn('Preencha todos os dados');
+      return;
+    }
+
+    setLoading(true);
+
+    let data = {
+      email,
+      password
+    }
+
+    await signIn(data);
+
+    setLoading(false);
+
+  }
+
+  return (
+    <>
+    <Head>
+      <title>***  Faça seu login</title>
+    </Head>
+
+    <div className={styles.containerCenter}>
+      <Image src={logoImg} alt="Logo do Projeto" />
+
+      <div className={styles.login}>
+        <form onSubmit={handleLogin}>
+          <Input 
+            placeholder="Digite seu email" 
+            type="text"
+            value={email}
+            onChange={ (e) =>  setEmail(e.target.value) }
+          />
+
+          <Input
+            placeholder="Digite sua senha" 
+            type="password"
+            value={password}
+            onChange={ (e) =>  setPassword(e.target.value) }
+          />
+
+          <Button type="submit" loading={loading}>Acessar</Button>
+
+        </form>
+
+        <Link href="/signup"  className={styles.text}> Não possui uma conta? Cadastre-se
+        </Link>
+      </div>
+    </div>
+    </>
+  )
+}
+
+
+export const getServerSideProps =  canSSRGuest(async (ctx) => {
+  return{
+    props: {}
+  }
+})
+
